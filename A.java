f66874b31646f832643307dd9b002c6392d98e93
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-08-24 17:14:41 +0530

    Code

--- A.java
+++ A.java
@@ -1,6 +1,24 @@
 class B{
-    public static void main(String args[]){
-        System.out.println("Hello Java");
+    static public  void main(String ...arr){
+        D.show();
+        D.print();
+        final int MAX_AGE = 100;
+       // System s = new System();
+        Math.abs(-99);
+       // MAX_AGE++;
+       System.out.println("Hello");
+       // System.out.println("Hello Java");
 
     }
+}
+
+class D{
+    static void show(){
+        System.out.println("Hello Show");
+
+    }
+static void print(){
+    System.out.println("Hello Print");
+
+}
 }
---
+++ FirstDemo.java
@@ -0,0 +1,7 @@
+class FirstDemo{
+    public static void main(String[] args) {
+        int x = 100;
+        byte a = (byte)130;
+        System.out.println(a);
+    }
+}
